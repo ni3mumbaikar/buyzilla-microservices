982fb8d4e40fedb051c1475315a66f19f4a4660f
ni3mumbaikar <nitinkale035@gmail.com>
2023-01-23 02:55:46 +0530

    Add : Spring Cloud Gateway

---
+++ buyzilla-api-gateway-service/src/main/java/com/buyzilla/services/buyzillaapigateway/BuyzillaApiGatewayApplication.java
@@ -0,0 +1,29 @@
+package com.buyzilla.services.buyzillaapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BuyzillaApiGatewayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BuyzillaApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+
+		return builder.routes()
+				.route(p -> p
+						.path("/api/v1/products")
+						.uri("http://localhost:2030/api/v1/products")
+				).route(p -> p
+				.path("/api/v1/suppliers")
+				.uri("http://localhost:2031/api/v1/suppliers")
+		)
+				.build();
+	}
+
+}

5351735455ef5996e43f83389e27cd68aa67fbbc
ni3mumbaikar <nitinkale035@gmail.com>
2023-02-08 10:45:45 +0530

    Add : Unversioned files for shipper

---
+++ buyzilla-shipper-service/src/test/java/com/buyzilla/services/buyzillashipperservice/BuyzillaShipperServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buyzilla.services.buyzillashipperservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyzillaShipperServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
